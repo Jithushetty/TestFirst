962ef25888eb6a76c9c3db842ea0e12715fec36d
jithu <jithu>
2017-04-01 18:35:34 +0530

    myfirsth

--- app.js
+++ app.js
@@ -3,7 +3,8 @@
   'use strict';
   angular.module('myFirstapp', []);
 
-  .controller('MyFirstController',function(){
+  .controller('MyFirstController',function($scope){
+    $scope.name="Jithu";
 
 });
 })();
